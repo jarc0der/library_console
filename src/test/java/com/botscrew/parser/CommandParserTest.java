3d30b53ea99488339ac24ab9d1da8af987369f4c
Yaroslav_Tuzhanskyi <yaroslav.tuzhanskyi@epam.com>
2017-10-13 16:57:56 +0300

    update command parser test

--- src/test/java/com/botscrew/parser/CommandParserTest.java
+++ src/test/java/com/botscrew/parser/CommandParserTest.java
@@ -2,10 +2,17 @@ package com.botscrew.parser;
 
 import com.botscrew.library.utils.parser.CommandParser;
 import com.botscrew.library.utils.parser.ParsedCommand;
+import com.tngtech.java.junit.dataprovider.DataProvider;
+import com.tngtech.java.junit.dataprovider.DataProviderRunner;
+import com.tngtech.java.junit.dataprovider.UseDataProvider;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 
+@RunWith(DataProviderRunner.class)
 public class CommandParserTest {
 
     private CommandParser parser = new CommandParser();
@@ -41,14 +48,31 @@ public class CommandParserTest {
     }
 
     @Test
-    public void onlySimpleCommandTest(){
-        String command = "command1 param1 param2";
+    @UseDataProvider("simpleCommands")
+    public void onlySimpleCommandTest(String input, String expectedCommand, String param, int expectedParamsCount){
+        ParsedCommand command1 = parser.parse(input);
 
-        ParsedCommand command1 = parser.parse(command);
+        assertEquals(expectedCommand, command1.getCommandName());
 
-        assertEquals("command1", command1.getCommandName());
-        assertEquals(2, command1.getParams().size());
+        assertEquals(param, command1.getParams().get(0));
+
+        assertEquals(expectedParamsCount, command1.getParams().size());
     }
 
+    @DataProvider
+    public static Object[][] simpleCommands(){
+        return new Object[][]{
+                {
+                    "com param1",
+                    "param1",
+                    1
+                },
+                {
+                    "com 'param1'",
+                    "param1",
+                    1
+                },
+        };
+    }
 
 }
